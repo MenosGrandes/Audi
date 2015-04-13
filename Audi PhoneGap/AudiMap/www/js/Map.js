91e28b4f0b50b9e6d33b83dd76a34d3b05e19a2b
MenosGrandes <kaczor201@o2.pl>
2015-04-13 16:54:44 +0200

    Klasy poprawione. InfoWindow, czyli jak kliknie sie w Marker jest zrypane. Teraz poprawiam te InfoWindow. TODO: Poprawic pojawianie sie InfoWindow. Laczenie Markerow i rysowanie drogi

--- Audi PhoneGap/AudiMap/www/js/Map.js
+++ Audi PhoneGap/AudiMap/www/js/Map.js
@@ -1,72 +1,76 @@
 var Map = {
-		/*Google Map Object*/
-var googleMap:null,
-/*Array of Waypoints.Start Waypoint is on 0 index, last on last*/
-var waypointsArray = [],
-/**/
-		onSuccessLoad : function(position) {
-			var longitude = position.coords.longitude;
-			var latitude = position.coords.latitude;
-			var latLong = new google.maps.LatLng(latitude, longitude);
+	/* Google Map Object */
+	googleMap : null,
+	/* Array of Waypoints.Start Waypoint is on 0 index, last on last */
+	waypointsArray : [],
+	/**/
 
-			var mapOptions = {
-				center : latLong,
-				zoom : 3,
-				mapTypeId : google.maps.MapTypeId.ROADMAP
-			};
+	/**/
+	addWaypoint : function(latLong) {
+		/*
+		 * var infowindow = new google.maps.InfoWindow({ content: hour + ":" +
+		 * min + ":" + seconds });
+		 */
+		var marker = new google.maps.Marker({
+			position : latLong,
+			map : Map.googleMap,
+			title : "New Marker"
+		});
+		/*
+		 * google.maps.event.addListener(marker, 'click', function() {
+		 * infowindow.open(Map.googleMap,marker); });
+		 */
+		Map.waypointsArray.push(marker);
+	},
+	/**/
+	addWaypoint : function(latLong, _icon) {/*
+											 * var infowindow = new
+											 * google.maps.InfoWindow({ content:
+											 * hour + ":" + min + ":" + seconds
+											 * });
+											 */
+		var marker = new google.maps.Marker({
+			position : latLong,
+			map : Map.googleMap,
+			title : "New Marker",
+			icon : _icon
+		});
+		/*
+		 * google.maps.event.addListener(marker, 'click', function() {
+		 * infowindow.open(Map.googleMap,marker); });
+		 */
+		Map.waypointsArray.push(marker);
+	},
 
-			this.googleMap = new google.maps.Map(document.getElementById("map"),
-					mapOptions);
-			//addWaypoint(latLong,'img/startNode.png');
-			
-		},
-		/**/
-		onSuccessRefresh : function(position) {
-			var date = new Date();
-			var hour = date.getHours();
-			var min = date.getMinutes();
-			var seconds = date.getSeconds();
+	/**/
+	onSuccessLoad : function(position) {
+		var longitude = position.coords.longitude;
+		var latitude = position.coords.latitude;
+		var latLong = new google.maps.LatLng(latitude, longitude);
 
-			var longitude = position.coords.longitude;
-			var latitude = position.coords.latitude;
-			var latLong = new google.maps.LatLng(latitude, longitude);
-			//addWaypoint(latLong);
+		var mapOptions = {
+			center : latLong,
+			zoom : 25,
+			mapTypeId : google.maps.MapTypeId.ROADMAP
+		};
 
-		},
-		/**/
-		addWaypoint : function(latLong)
-		{
-			var infowindow = new google.maps.InfoWindow({
-			      content: hour + ":" + min + ":" + seconds
-			  });
-			var marker = new google.maps.Marker({
-				position : latLong,
-				map : this.googleMap,
-				title : "New Marker"
-			});
-			
-			  google.maps.event.addListener(marker, 'click', function() {
-				    infowindow.open(this.googleMap,marker);
-				  });
-			this.waypointsArray.push(marker);
-		},
-		/**/
-		addWaypoint : function(latLong,_icon)
-		{
-			var infowindow = new google.maps.InfoWindow({
-			      content: hour + ":" + min + ":" + seconds
-			  });
-			var marker = new google.maps.Marker({
-				position : latLong,
-				map : this.googleMap,
-				title : "New Marker",
-				icon : _icon
-			});
-			
-			  google.maps.event.addListener(marker, 'click', function() {
-				    infowindow.open(this.googleMap,marker);
-				  });
-			  this.waypointsArray.push(marker);
-		}
-		
+		Map.googleMap = new google.maps.Map(document.getElementById("map"),
+				mapOptions);
+		Map.addWaypoint(latLong, 'img/startNode.png');
+
+	},
+	/**/
+	onSuccessRefresh : function(position) {
+		var date = new Date();
+		var hour = date.getHours();
+		var min = date.getMinutes();
+		var seconds = date.getSeconds();
+
+		var longitude = position.coords.longitude;
+		var latitude = position.coords.latitude;
+		var latLong = new google.maps.LatLng(latitude, longitude);
+		Map.addWaypoint(latLong);
+
+	}
+
 }
